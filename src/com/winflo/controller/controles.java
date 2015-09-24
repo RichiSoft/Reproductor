fb71ab99d956bc7f6a54a8a7ca0fa899b6575e44
RichiSoft <RichiSoft@RSystem>
2015-09-24 10:49:27 +0200

    Modificado Abrir archivo: añadido jFileChoser
    Modificado controles:
    añadidas funciones vacias

--- src/com/winflo/controller/controles.java
+++ src/com/winflo/controller/controles.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *  Clase controladores:
+ *  Se encarga de las funciones del reproductor: Reproducir, rebobinar, pausar y parar.
+ * 
  */
 package com.winflo.controller;
 
-/**
- *
- * @author RichiSoft
- */
 public class controles {
     
+    /**
+     * Funcionalidades de los botones.
+     */
+    public void Play(){
+        
+    }
+    
+    public void Pause(){
+        
+    }
+    
+    public void Rewind(){
+        
+    }
+    
+    public void Fast(){
+        
+    }
+    
+    public void Stop(){
+        
+    }
+    
+    public void VolUp(){
+        
+    }
+    
+    public void VolDown(){
+        
+    }
 }
